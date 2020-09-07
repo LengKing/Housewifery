fbb3563531dcf91f8991f3c98de9cc2aca5f2e94
Administrator <596682714@qq.com>
2020-09-07 14:21:19 +0800

    housewifery

---
+++ src/main/java/com/cykj/housewifery/HousewiferyApplication.java
@@ -0,0 +1,13 @@
+package com.cykj.housewifery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousewiferyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousewiferyApplication.class, args);
+    }
+
+}
