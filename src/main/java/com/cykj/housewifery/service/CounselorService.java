453ad83677835eda69507bbe94a919c62fe2bc06
Administrator <596682714@qq.com>
2020-09-15 14:27:24 +0800

    顾问管理

--- src/main/java/com/cykj/housewifery/service/CounselorService.java
+++ src/main/java/com/cykj/housewifery/service/CounselorService.java
@@ -1,5 +1,12 @@
 package com.cykj.housewifery.service;
 
+import com.cykj.housewifery.bean.Counselor;
+
+import java.util.List;
+
 public interface CounselorService {
+
+    int getCounselorCount();
+    List<Counselor> selCounselor(Integer pageNum, String limit);
 
 }
--- src/main/java/com/cykj/housewifery/service/impl/CounselorServiceImpl.java
+++ src/main/java/com/cykj/housewifery/service/impl/CounselorServiceImpl.java
@@ -1,10 +1,28 @@
 package com.cykj.housewifery.service.impl;
 
+import com.cykj.housewifery.bean.Counselor;
+import com.cykj.housewifery.mapper.CounselorMapper;
 import com.cykj.housewifery.service.CounselorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("counselorService")
 public class CounselorServiceImpl implements CounselorService {
 
+    @Autowired
+    private CounselorMapper counselorMapper;
 
+    @Override
+    public int getCounselorCount() {
+        int count =counselorMapper.getCounselorCount();
+        return count;
+    }
+
+    @Override
+    public List<Counselor> selCounselor(Integer pageNum, String limit) {
+        List<Counselor> counselors=counselorMapper.selCounselor(pageNum,Integer.valueOf(limit));
+        return counselors;
+    }
 }
